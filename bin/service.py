ce681ab482483c1ca11b349cead4fa73b5333fb8
Dmitrii Khodakov <dmitryhd@gmail.com>
2018-04-05 16:20:11 +0300

    Refactor service

--- bin/service.py
+++ bin/service.py
@@ -3,9 +3,14 @@
 import os.path as path
 import sys
 
-if __name__ == '__main__':
+
+def main():
     cur_dir = path.dirname(path.abspath(__file__))
     sys.path.append(path.join(cur_dir, '..'))
     import avio.application
     app = avio.application.make_app()
     avio.application.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
